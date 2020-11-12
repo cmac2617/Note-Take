5edf99d0df6fd9071143e1bd5fe956b278624eea
Cyle <cmac2617@yahoo.com>
2020-11-11 21:38:27 -0500

    Readme.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,7 +8,7 @@ router.get("/notes", (req, res) => {
     .catch(err => res.status(500).json(err));
 })
 
-router.post("/notes/:id", (req, res) => {
+router.post("/notes/", (req, res) => {
     store
     .addNote(req.body)
     .then((note) => res.json(note))
